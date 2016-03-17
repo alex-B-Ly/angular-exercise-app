c3f6ebc8c712dfdb58e9e0ffe8bfd64b843e3075
Alex Ly <alexly@nbp-158-32.nbp.ruw.rutgers.edu>
2016-03-17 19:47:02 -0400

    Basic Exercise functionality

--- swole.js
+++ swole.js
@@ -4,4 +4,15 @@ angular.module('swole-app', [])
 
   $scope.exerciseGroups = ['Chest', 'Shoulders', 'Back', 'Legs', 'Arms'];
   $scope.workouts = [];
+
+  $scope.addWorkout = function(){
+    $scope.workouts.push({
+      muscleGroup: $scope.exercise.muscleGroup,
+      name: $scope.exercise.name,
+      date: $scope.exercise.date,
+      weight: $scope.exercise.weight,
+      reps: $scope.exercise.reps
+    });
+    $scope.exercise = {};
+  }
 });
